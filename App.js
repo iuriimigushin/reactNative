7e3766cb3d2b44a0988308433717dc17878337a9
Yuri Migushin <imigushin@gmail.com>
2021-11-07 15:45:26 +0300

    Fixed 'onPress' action by transform attr => func.

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const handleTextPress = () => console.log('Text pressed');
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text} onPress={() => console.log('Text pressed')}>Hello World!</Text>
+      <Text style={styles.text} onPress={handleTextPress}>Hello World!</Text>
 
       <StatusBar style="auto" />
     </SafeAreaView>
