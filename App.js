eb867fcbc22ec233516c520cfa96cf8ee754e81b
Yuri Migushin <imigushin@gmail.com>
2021-11-07 16:26:26 +0300

    Added some notice.

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
-  const handleTextPress = () => console.log('Text pressed');
+  const handleTextPress = () => console.log('Text output within function used.');
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text} onPress={handleTextPress}>Hello World!</Text>
+      <Text style={styles.text}>It's funny Text-entity don't render without link to corresponding style.</Text>
+      <Text style={styles.text}>SafeAreaView entity doesn't work on Android 6 Xiaomi Mi3W.</Text>
+      <Text style={styles.text} onPress={console.log('Text output within attribute used.')}>Text output withing attribute.</Text>
+      <Text style={styles.text} onPress={handleTextPress}>Text output within function.</Text>
       <Button title={'Press on me'} onPress={handleTextPress} color="green" />
 
       <StatusBar style="auto" />
