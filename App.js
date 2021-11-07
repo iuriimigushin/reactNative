ce9b219bd1c54839f6dc0ec8736ebf5e7389b95d
Yuri Migushin <imigushin@gmail.com>
2021-11-07 16:28:29 +0300

    Fixed 'onPress' call as attribute.

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>It's funny Text-entity don't render without link to corresponding style.</Text>
       <Text style={styles.text}>SafeAreaView entity doesn't work on Android 6 Xiaomi Mi3W.</Text>
-      <Text style={styles.text} onPress={console.log('Text output within attribute used.')}>Text output withing attribute.</Text>
+
+      <Text style={styles.text} onPress={() => console.log('Text output within attribute used.')}>Text output withing attribute.</Text>
       <Text style={styles.text} onPress={handleTextPress}>Text output within function.</Text>
       <Button title={'Press on me'} onPress={handleTextPress} color="green" />
 
