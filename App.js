81114867c529d0b2f3cdb504af29f4442d83226d
Yuri Migushin <imigushin@gmail.com>
2021-11-07 14:32:19 +0300

    Added attr. "onPress".

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+      <Text style={styles.text} onPress={() => console.log('Text pressed')}>Hello World!</Text>
 
       <StatusBar style="auto" />
     </SafeAreaView>
